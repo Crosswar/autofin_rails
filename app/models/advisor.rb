8f3a1fce49f3a14f0f7b87128715722c12b7184a
Crosswar <string.gabriel@gmail.com>
2016-10-08 00:47:54 -0300

    Better Approach to model

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -2,6 +2,8 @@ class Advisor
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :is_admin, type: Mongoid::Boolean, default: false
+
   field :advisor_code, type: String
   field :advisor_name, type: String
   field :advisor_email, type: String
--- app/models/office.rb
+++ app/models/office.rb
@@ -2,10 +2,6 @@ class OfficeSetup
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  #Office information - Users
-  field :office_admin, type: String
-  field :office_admin_advisor_code, type: String
-
   #Office Information - General
   field :client_data_last_import, type: Time
 
