b1cafdbfc9f42e8942fd2c424f66227a7a614dba
Crosswar <string.gabriel@gmail.com>
2016-10-13 23:24:45 -0300

    Clients Features, Scopes for Active/Inactives and fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   respond_to :html
 
   def index
+  	respond_with @clients = Client.all
   end
 
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -4,28 +4,34 @@ class Client
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :client_advised_by, type: String
-  field :client_code, type: String
-  field :client_name, type: String
-  field :client_dob, type: Date
-  field :client_email, type: String
-  field :client_amount_invested, type: BigDecimal
-  field :client_amount_unused, type: BigDecimal
+  field :advised_by, type: String
+  field :code, type: String
+  field :name, type: String
+  field :dob, type: Date
+  field :status, type: String
+  field :email, type: String
+  field :amount_invested, type: BigDecimal
+  field :amount_unused, type: BigDecimal
 
   belongs_to :user
+
+  scope :active, -> { where(status: "ATIVO")}
+  scope :inactive, -> { where(status: "INATIVO")}
+  scope :total_unused, -> { where(:amount_unused.gt => 0)}
 
   def self.import(file)
     Client.delete_all
     spreadsheet = open_spreadsheet(file)
     spreadsheet.each_row_streaming(pad_cells: true, offset: 1) do |row|
       attributes = {
-        :client_advised_by => row[0].try(:cell_value),
-        :client_code => row[1].try(:cell_value),
-        :client_name => row[3].try(:cell_value),
-        :client_dob => row[11].try(:cell_value),
-        :client_email => row[7].try(:cell_value),
-        :client_amount_invested => row[34].try(:cell_value),
-        :client_amount_unused => row[38].try(:cell_value)
+        :advised_by => row[0].try(:cell_value),
+        :code => row[1].try(:cell_value),
+        :name => row[3].try(:cell_value),
+        :dob => row[11].try(:cell_value),
+        :status => row[12].try(:cell_value),
+        :email => row[7].try(:cell_value),
+        :amount_invested => row[34].try(:cell_value),
+        :amount_unused => row[38].try(:cell_value)
       }
       client = Client.new
       client.attributes = attributes
